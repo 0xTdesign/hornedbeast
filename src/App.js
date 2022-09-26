5e095a7ecf871ada2a622238ea06fde40d2007a7
jimbobdxb <tallisondxb@gmail.com>
2022-09-26 19:22:36 +0400

    first React lab-1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import "./App.css";
+import Header from "./components/Header/Header";
+import Main from "./components/Main/Main";
+import Footer from "./Footer/Footer";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
